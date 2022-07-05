8124071d6293b1ad4d3c84a12f2401e0b7918845
이운형 <wh619@gmail.com>
2022-07-05 17:54:28 +0900

    2022-07-05 ABAP select from where

---
+++ src/main/ABAP/TEST/TEST.java
@@ -0,0 +1,21 @@
+package TEST;
+@SLF4J
+
+public class TEST {
+
+    int value = 1;
+
+    switch(value){
+        case 1:
+            System.out.println("1");
+            break;
+        case 2:
+            System.out.println("2");
+            break;
+        case 3 :
+            System.out.println("3");
+            break;
+        default :
+            System.out.println("그 외의 숫자");
+    }
+}
